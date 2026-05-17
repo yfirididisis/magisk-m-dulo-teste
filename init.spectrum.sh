be9eb9912ec746301e75b09f98ce0ad49561eaa1
yfirididisis <ffydhfmmrsgh@gmail.com>
2026-05-17 19:03:31 -0300

    Delete init.spectrum.sh

--- init.spectrum.sh
+++
@@ -1,9 +0,0 @@
-#!/system/bin/sh
-
-# Wait for the system to fully boot
-until [ "$(getprop sys.boot_completed)" = "1" ]; do
-    sleep 2
-done
-
-# Set the default profile (0 = Balance)
-setprop persist.spectrum.profile 0
